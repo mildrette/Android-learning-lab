d55810dc697539ae6bd162a1412071e179700397
Mildrette <mildredfonka1@gmail.com>
2026-06-29 12:21:57 +0100

    Add button variants to WelcomeView

--- app/src/main/java/com/example/learningkotlin/ui/WelcomeView.kt
+++ app/src/main/java/com/example/learningkotlin/ui/WelcomeView.kt
@@ -1,10 +1,18 @@
 package com.example.learningkotlin.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -13,12 +21,40 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun WelcomeView() {
-    Row(modifier = Modifier
-        .fillMaxSize()
-        .background(Color.White)
-        .padding(16.dp)
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(16.dp)
     ) {
-        Text("Welcome to My Todo list App")
+        Column{
+            Text("Welcome to My Todo list App")
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(onClick = {}) {
+                Text("Get Started")
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            FilledTonalButton(onClick = {}) {
+                Text("Tonal Button")
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedButton(onClick = {}) { Text("Outlined Buttons") }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            ElevatedButton(onClick = {}) { Text("Elevated Buttons") }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            TextButton(onClick = {}) { Text("Text Button") }
+        }
+
     }
 }
 
