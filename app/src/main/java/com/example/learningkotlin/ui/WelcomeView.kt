e2f1c778790cafefdd81ef792e3d03765fe4850a
Mildrette <mildredfonka1@gmail.com>
2026-06-29 11:58:07 +0100

    Add WelcomeView composable with full screen layout

--- app/src/main/java/com/example/learningkotlin/ui/WelcomeView.kt
+++ app/src/main/java/com/example/learningkotlin/ui/WelcomeView.kt
@@ -1,2 +1,29 @@
-package com.example.learningkotlin.ui.theme
+package com.example.learningkotlin.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WelcomeView() {
+    Row(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.White)
+        .padding(16.dp)
+    ) {
+        Text("Welcome to My Todo list App")
+    }
+}
+
+@Preview
+@Composable
+fun WelcomeViewPreview() {
+    WelcomeView()
+}
